b04382ff9fbc6ae34cdddd8ca993b61feffdab10
April Sun <aprilsun711@gmail.com>
2024-04-02 18:39:32 -0400

    yippeeee

--- Driver.java
+++ Driver.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Driver {
-   Driver() {
-      Roster r = new Roster();
+
+   Scanner s;
+   Driver () {
+      s = new Scanner(System.in);
+   }
+   public void input() {
+
+      String option;
+      boolean exit = false;
+
+
+      System.out.println("Please enter the name of the txt file containing the player data: ");
+      String fileName = s.nextLine();
+      Roster r = new Roster(fileName);
+
+      while (!exit) {
+         System.out.println("Please enter an option (o for a list of options): ");
+         option = s.nextLine();
+         if (option.equals("o")) {
+            System.out.println("Options: ");
+            System.out.println("1. Sort by goals");
+            System.out.println("2. Sort by total points");
+            System.out.println("3. Quit");
+         } else if (option.equals("1")) {
+            r.sortGoals();
+            r.display();
+         } else if (option.equals("2")) {
+            r.sortPoints();
+            r.display();
+         } else if (option.equals("3")) {
+            exit = true;
+            System.out.println("Thank you for using this program.");
+         } else {
+            System.out.println("Please enter a valid option.");
+         }
+      }
+   }
+
+   public static void main(String args[]) {
+      Driver d = new Driver();
+      d.input();
    }
 }
--- Roster.java
+++ Roster.java
@@ -10,18 +10,18 @@ import java.io.*;
 public class Roster{
 
 	HockeyPlayer players[];
-	Scanner s;
-   
-	Roster(){									//to simplify for this program:
+
+
+	Roster(){                                    //to simplify for this program:
 		players = new HockeyPlayer[18]; //a hockey team will always have 18 skaters on the roster
-	   s = new Scanner(System.in);
+
    }
-	
+
 	Roster(String f){
 		players = new HockeyPlayer[18];
 		loadRoster(f);
 	}
-	
+
 	//utility method to load players from a file
 	private void loadRoster(String filename){
 		Scanner s;
@@ -29,21 +29,20 @@ public class Roster{
 		try{
 			f = new File (filename);
 			s = new Scanner(f);
-			
+
 			for(int i = 0; i < 18; i++){
 				String line = s.nextLine();
 				String temp[] = line.split (",");
 				//file format: name, goals, assists
-				this.players[i] = new HockeyPlayer (temp [0], Integer.parseInt(temp [1]), Integer.parseInt(temp [2])); 
+				this.players[i] = new HockeyPlayer (temp [0], Integer.parseInt(temp [1]), Integer.parseInt(temp [2]));
 			}
-		}
-		catch (Exception e){
+		} catch (Exception e){
 			System.out.println (e.getMessage());
 		}
-		
+
 		return;
 	}
-	
+
 	public void display(){
 		//display the roster using the player to string methods
 		for (HockeyPlayer i : players){
@@ -51,38 +50,40 @@ public class Roster{
 		}
 		return;
 	}
-   
-	
+
+
 	//insert assignment methods below this point
    
    public void sortPoints() {
-      
+	   int index = 0;
+	   while (index < 18) {
+		   if (index == 0)
+			   index++;
+		   if (players[index].getPoints() >= players[index - 1].getPoints())
+			   index++;
+		   else {
+			   HockeyPlayer temp = players[index];
+			   players[index] = players[index - 1];
+			   players[index - 1] = temp;
+			   index--;
+		   }
+	   }
    }
    
-   public void sortYear() {
-   
-   }
-   
-   public void input() {
-      String option;
-      System.out.println("Please enter the name of the txt file containing the player data: ");
-      String fileName = s.nextLine();
-      System.out.println("Please enter an option (o for a list of options): ");
-      option = s.nextLine();
-      if (option.equals("o")) {
-         System.out.println("Options: ");
-         System.out.println("1. Sort by goals");
-         System.out.println("2. Sort by total points");
-      }
-      else if (option.equals("1")) {
-      
-      }
-      else if (option.equals("2")) {
-      
-      }
-      else {
-         System.out.println("Please enter a valid option.");
-      }
+   public void sortGoals() {
+	   int index = 0;
+	   while (index < 18) {
+		   if (index == 0)
+			   index++;
+		   if (players[index].getGoals() >= players[index - 1].getGoals())
+			   index++;
+		   else {
+			   HockeyPlayer temp = players[index];
+			   players[index] = players[index - 1];
+			   players[index - 1] = temp;
+			   index--;
+		   }
+	   }
    }
    
 
